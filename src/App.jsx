3fa4379394056913b845bfd245e8f89bf5c879f7
AlexeyKonstantinov78 <alex_kav@mail.ru>
2022-12-19 21:10:07 +0300

    init project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { Container } from './components/container/Container';
+
+function App() {
+  return (
+    <Container>
+      Привет
+    </Container>
+  );
+}
+
+export default App;
