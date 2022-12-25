3bbd51267041f246371aec695e9d457975c8854e
AlexeyKonstantinov78 <alex_kav@mail.ru>
2022-12-25 10:56:00 +0300

    update fix

--- src/components/photos/Photos.jsx
+++ src/components/photos/Photos.jsx
@@ -9,6 +9,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { deleteLikeAsync, updateLikeAsync } from '../../store/likeReducer';
 import { photosAsync } from '../../store/photos/action';
 
+const withHeigt = (w, h) => (400 / (w / h));
 
 export const Photos = ({ photo }) => {
   const token = useSelector((state) => state.token.token);
@@ -27,8 +28,15 @@ export const Photos = ({ photo }) => {
     dispatch(photosAsync(1));
   };
 
+  const styleImg = {
+    height: withHeigt(photo.width, photo.height),
+    objectFit: 'cover'
+  };
+
   return (
-    <div className={_.photos_div} key={photo.blur_hash}>
+    <div
+      className={_.photos_div}
+      key={photo.blur_hash}>
       <Link to={`/photos/${photo.id}`}>
         <data
           className={_.photos__date}
@@ -37,11 +45,11 @@ export const Photos = ({ photo }) => {
           {formDate(photo.updated_at)}
         </data>
         <img
+          style={styleImg}
           className={_.photos_img}
-          src={photo.urls.regular}
+          src={photo.urls.small}
           alt={photo.description}
         />
-
       </Link>
       <div className={_.photos_autors}>
         <img
@@ -49,12 +57,15 @@ export const Photos = ({ photo }) => {
           src={photo.user.profile_image.small}
           alt={photo.user.name}
         />
-        <a className={_.photos_autors_link} href={photo.user.links.html} target='_blank'>
+        <a
+          className={_.photos_autors_link}
+          href={photo.user.links.html}
+          target='_blank'>
           {photo.user.name}
         </a>
       </div>
       <div className={_.photos__like}>
-        <strong title={photo.likes} >{photo.likes}</strong>
+        <strong title={photo.likes}>{photo.likes}</strong>
         <ButtonLIkeIcon
           className={_.photos__like_svg}
           aria-checked={photo.liked_by_user}
